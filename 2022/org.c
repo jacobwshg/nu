f8b79105d6a622d43e70d7ea82f0c7e435f2ac4e
jacobwshg <wsg2002@hotmail.com>
2025-09-06 17:58:23 -0500

    updated type name

--- 2022/org.c
+++ 2022/org.c
@@ -2,12 +2,12 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-struct person
+struct empl
 {
     char *name;
     char *title;
     int salary;
-    struct person **subords;
+    struct empl **subords;
     int num_subords;
 };
 
@@ -20,31 +20,31 @@ struct sal_data
 struct sal_inv_pair
 {
     bool found;
-    struct person *director;
-    struct person *subord;
+    struct empl *director;
+    struct empl *subord;
 };
 
-typedef struct person *org_t;
+typedef struct empl *org_t;
 
-void person_init(struct person *, char *, char *, int, struct person **, int);
+void empl_init(struct empl *, char *, char *, int, struct empl **, int);
 void try_update_min_sal_data(int, char *, struct sal_data *);
-void get_dept_min_sal_data(struct person *, struct sal_data *);
-void find_dept_sal_inv(struct person *, struct sal_inv_pair *);
+void get_dept_min_sal_data(struct empl *, struct sal_data *);
+void find_dept_sal_inv(struct empl *, struct sal_inv_pair *);
 void example(void);
 
 void
-person_init(struct person *person, char *name, char *title, 
-    int salary, struct person **subords, int nsubords)
+empl_init(struct empl *empl, char *name, char *title, 
+    int salary, struct empl **subords, int nsubords)
 {
-    if (!person)
+    if (!empl)
     {
         return;
     }
-    person->name = name;
-    person->title = title;
-    person->salary = salary;
-    person->subords = subords;
-    person->num_subords = nsubords;
+    empl->name = name;
+    empl->title = title;
+    empl->salary = salary;
+    empl->subords = subords;
+    empl->num_subords = nsubords;
 }
 
 void
@@ -63,7 +63,7 @@ try_update_min_sal_data(int salary, char *name,
 }
 
 // Update MSDATA to lowest salary and holder within DIRECTOR's department
-void get_dept_min_sal_data(struct person *director, 
+void get_dept_min_sal_data(struct empl *director, 
     struct sal_data *msdata)
 {
     if (!director || !msdata)
@@ -73,7 +73,7 @@ void get_dept_min_sal_data(struct person *director,
     // Visit director's node
     try_update_min_sal_data(director->salary, director->name, msdata);
 
-    struct person **subords = director->subords;
+    struct empl **subords = director->subords;
     if (subords)
     {
         int nsubords = director->num_subords;
@@ -86,13 +86,13 @@ void get_dept_min_sal_data(struct person *director,
 }
 
 void 
-find_dept_sal_inv(struct person *director, struct sal_inv_pair *invpair)
+find_dept_sal_inv(struct empl *director, struct sal_inv_pair *invpair)
 {
     if (!director || !invpair)
     {
         return;
     }
-    struct person **subords = director->subords;
+    struct empl **subords = director->subords;
     if (!subords)
     {
         return;
@@ -104,7 +104,7 @@ find_dept_sal_inv(struct person *director, struct sal_inv_pair *invpair)
         {
             return;
         }
-        struct person *sub = subords[i];
+        struct empl *sub = subords[i];
         printf("Checking salary inversion between %s and %s\n",
             director->name, sub->name);
         if (sub->salary > director->salary)
@@ -124,25 +124,25 @@ find_dept_sal_inv(struct person *director, struct sal_inv_pair *invpair)
 void
 example(void)
 {
-    struct person turner, jacobs, jones, jacobsen,
+    struct empl turner, jacobs, jones, jacobsen,
         jensen, thompson, jordan, jennings, johnson;
-    struct person *subs_thompson[1], *subs_jordan[2], *subs_jennings[2],
+    struct empl *subs_thompson[1], *subs_jordan[2], *subs_jennings[2],
         *subs_johnson[3];
 
-    person_init(&turner, "Theresa Turner", "Backend Dev", 250000, NULL, 0);
-    person_init(&jacobs, "Joyce Jacobs", "IT Staff", 90000, NULL, 0);
-    person_init(&jones, "Jane Jones", "IT Staff", 80000, NULL, 0);
-    person_init(&jacobsen, "Juliet Jacobsen", "Account Manager", 200000, NULL, 0);
-    person_init(&jensen, "Josie Jensen", "Account Manager", 200000, NULL, 0);
-    person_init(&thompson, "Tina Thompson", "CIO", 1300000, subs_thompson, 1);
+    empl_init(&turner, "Theresa Turner", "Backend Dev", 250000, NULL, 0);
+    empl_init(&jacobs, "Joyce Jacobs", "IT Staff", 90000, NULL, 0);
+    empl_init(&jones, "Jane Jones", "IT Staff", 80000, NULL, 0);
+    empl_init(&jacobsen, "Juliet Jacobsen", "Account Manager", 200000, NULL, 0);
+    empl_init(&jensen, "Josie Jensen", "Account Manager", 200000, NULL, 0);
+    empl_init(&thompson, "Tina Thompson", "CIO", 1300000, subs_thompson, 1);
     subs_thompson[0] = &turner;
-    person_init(&jordan, "Jessica Jordan", "CTO", 600000, subs_jordan, 2);
+    empl_init(&jordan, "Jessica Jordan", "CTO", 600000, subs_jordan, 2);
     subs_jordan[0] = &jacobs; 
     subs_jordan[1] = &jones;
-    person_init(&jennings, "Julia Jennings", "CFO", 500000, subs_jennings, 2);
+    empl_init(&jennings, "Julia Jennings", "CFO", 500000, subs_jennings, 2);
     subs_jennings[0] = &jacobsen;
     subs_jennings[1] = &jensen;
-    person_init(&johnson, "Jasmine Johnson", "CEO", 1400000, subs_johnson, 3);
+    empl_init(&johnson, "Jasmine Johnson", "CEO", 1400000, subs_johnson, 3);
     subs_johnson[0] = &thompson;
     subs_johnson[1] = &jordan;
     subs_johnson[2] = &jennings;
